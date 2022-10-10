f429560981e645f6432b8a4fc99b22aba32ed654
Marla Gualdron <ma.gualdron@gmail.com>
2022-10-10 12:00:13 -0300

    changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,38 @@
 import React from "react";
 import "../../src/index.css";
 
-const person = { name: "Miguel", age: 25, position: "programador" };
+const person = [
+  { id: 1, name: "Marla", age: 24, position: "programador" },
+  { id: 2, name: "Miguel", age: 29, position: "programador" },
+  { id: 3, name: "Sole", age: 30, position: "programador" },
+  { id: 4, name: "Lucas", age: 30, position: "programador" },
+  { id: 5, name: "Nati", age: 30, position: "diseñador gráfico" },
+];
 
-const Card = () => {
+const CardListItem = (props) => {
   return (
-    <div className="person">
-      <h2>Name: {person.name}</h2>
-      <h3>Position: {person.position}</h3>
-      <p>Age: {person.age}</p>
+    <div className="card">
+      <h2>Name: {props.person.name}</h2>
+      <h3>Position: {props.person.position}</h3>
+      <p>Age: {props.person.age}</p>
     </div>
   );
 };
 
-export default Card;
+const CardList = () => {
+  return (
+    <ul style={{ listStyleType: "none" }}>
+      {person.map((person) => {
+        return <CardListItem person={person} key={person.id} />;
+      })}
+    </ul>
+  );
+};
+
+export default function Card() {
+  return (
+    <div className="Card">
+      <CardList />
+    </div>
+  );
+}
